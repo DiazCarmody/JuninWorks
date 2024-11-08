80235f0ce9ed2e1b1372bbcfd1cb664ab5182c8c
Santiago Henze <129229364+SantiagoHenze@users.noreply.github.com>
2024-11-08 01:52:41 -0300

    Delete scripts/chat.js

--- scripts/chat.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const chatsection = document.querySelector('.chatsection');
-    const closebutton = document.querySelector('.closeBTN');
-
-    closebutton.addEventListener('click', () => {
-        chatsection.style.display = 'none';
-    });
-
-});
